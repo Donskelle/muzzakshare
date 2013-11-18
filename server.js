dadb2723494b8fb73b68a96b0b598b5593444009
Donskelle <donskelle@googlemail.com>
2013-11-18 13:16:03 +0100

    Anfrage JOIN

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
   , playlist = require("./playlist");
+ 
 
 app.listen(8080);
 
@@ -17,46 +18,85 @@ function handler (req, res) {
     res.end(data);
   });
 }
-
+var users = new Array();
+var playlists = new Array();
+var rooms = new Array();
 
 io.sockets.on('connection', function (socket) {
+	//MongoClient.connect('mongodb://localhost/test/', function(err, db) {
+		var length = users.length;
+		users[length] = new Object();
+		users[length].userid = length;
+		users[length].online = true;
+		users[length].room = 0;
+		playlists[length] = new Object();
+		playlists[length] = require("./playlist");
+		var currentCategory = length;
 
-  socket.broadcast.emit('user', {user:"join"});
-  
-  socket.on('requestPlaylist', function(data)
-  {
-	socket.emit('playlistnew', playlist.tracks);
-  });
-  
-  socket.on('changeVolume', function(data) {
-  	socket.broadcast.emit('changeVolume', data);
-  });
-  
-  socket.on('disconnect', function () {
-	socket.broadcast.emit('user', {user:"leave"});
-  });
-  
-  socket.on('addTrack', function (data) {
-	console.log(data);
-	playlist.addTrack(data);
-	socket.broadcast.emit('addTrack', data);
-  });
-  socket.on('changevideonext', function() {
-	playlist.nextTrack();
-	socket.broadcast.emit('changevideonext', "test");
-  });
-  socket.on('changevideolast' ,function() {
-	playlist.lastTrack();
-	socket.broadcast.emit('changevideolast', "test");
-  });
-  socket.on('changevideo', function(data) {
-	socket.broadcast.emit('changevideo', data);
-	playlist.setCurrentTrack(data);
-  });
-  socket.on('playlistaddlink', function(data) {
-	playlist.addLink(data.url);
-	socket.broadcast.emit('playlistaddlink', { url: data.url });
-  });
-	
- });
+		
+		//socket.broadcast.emit('user', {user: currentuser});
+		socket.emit('currentuser', {user: length});
+		socket.broadcast.emit('user', {user: "join", userid: length});
+		console.log(length);
+		//db.testData.update({name: "Fabian", languages: ["test", "html"]});
 
+		socket.on('requestPlaylist', function(data)
+		{
+			socket.emit('playlistnew', playlists[currentCategory].tracks);
+			for (var i = 0; i < users.length; i++) {
+				if(users[i].online == true)
+					if(users[i].userid != length)
+						socket.emit('onlineuser', {userid: users[i].userid});
+			} 
+			
+		});
+		
+		socket.emit('acceptJoinRoom', function(data)
+		{
+			var currentLength = rooms.length;
+			rooms[currentLength] = new Object();
+			users[length].room = rooms;
+			socket.broadcast.emit = 
+		}
+		
+		socket.on('askJoinRoom', function(data) {
+			socket.broadcast.emit('askJoin', {to: data.userid, from: length});
+		});
+		
+		socket.on('changeVolume', function(data) {
+			socket.broadcast.emit('changeVolume', data);
+		});
+
+		socket.on('disconnect', function () {
+			socket.broadcast.emit('user', {user:"leave", userid: length});
+			users[length].online = false;
+		});
+
+		socket.on('addTrack', function (data) {
+			playlists[length].addTrack(data);
+			socket.broadcast.emit('addTrack', data);
+		});
+		
+		socket.on('changevideonext', function() {
+			playlists[length].nextTrack();
+			socket.broadcast.emit('changevideonext', "test");
+		});
+		
+		socket.on('debug', function() {
+			console.log(length);
+		});
+		socket.on('changevideolast' ,function() {
+			playlists[length].lastTrack();
+			socket.broadcast.emit('changevideolast', "test");
+		});
+		
+		socket.on('changevideo', function(data) {
+			socket.broadcast.emit('changevideo', data);
+			playlists[length].setCurrentTrack(data);
+		});
+		
+		socket.on('playlistaddlink', function(data) {
+			playlists[length].addLink(data.url);
+			socket.broadcast.emit('playlistaddlink', { url: data.url });
+		});
+});
